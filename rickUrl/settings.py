4cfffeab6322ae608f1114f44301610ad9143eb6
Moksh Sharma <mokshsharma@Mokshs-MacBook-Air.local>
2020-07-25 22:27:56 +0530

    try db

--- rickUrl/settings.py
+++ rickUrl/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -84,8 +84,10 @@ WSGI_APPLICATION = 'rickUrl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+DATABASES{
+    'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 
 # Password validation
